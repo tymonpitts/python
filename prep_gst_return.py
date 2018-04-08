7d2b19feeef038efbc29352e5e2d59b7bc91ffdd
Tymon Pitts <tpitts@wetafx.co.nz>
2018-04-08 21:49:08 +1200

    updated prep_gst_return.py to have better output and properly filter out some payees for large purchases and deposits

--- prep_gst_return.py
+++ prep_gst_return.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 """
 Parse YNAB csv file and do the following:
-- filter by date range
-- filter by real accounts (i.e. discard GST budget account)
-- output large transactions
+    - filter by date range
+    - filter by real accounts (i.e. discard GST budget account)
+    - output large transactions
 """
 import argparse
 import datetime
 import os
 
+from typing import List
+
 import ynab_csv
 
-
+# noinspection PyShadowingBuiltins
 def get_date(s, format='%d/%m/%Y'):
     return datetime.datetime.strptime(s, format).date()
 
@@ -28,15 +30,20 @@ def main(csv_file, output_file, from_date, to_date, threshold):
     ynab_file = ynab_csv.YnabCsvFile(csv_file)
     output_lines = [ynab_file.lines[0]]
     payees_to_skip = [
-        'Transfer : GST',
-        'Transfer : ANZ Credit',
-        'Transfer : ANZ Checking',
         'Iron Bridge',
         'Denise Le Cren',
+        'Direct Fx',
+        'IRD',
+        'ACC',
+        'GST',
+        'Immigration',
+        'AA',
         'Weta Digital',
+        'Commonsense Organics Wages',
+        'Starting Balance',
     ]
-    large_outflows = []  # type: list[ynab_csv.Item]
-    large_inflows = []  # type: list[ynab_csv.Item]
+    large_outflows = []  # type: List[ynab_csv.Item]
+    inflows = []  # type: List[ynab_csv.Item]
     for item in ynab_file.items:
         if item.account == 'GST':
             continue
@@ -48,11 +55,13 @@ def main(csv_file, output_file, from_date, to_date, threshold):
 
         if item.payee in payees_to_skip:
             continue
+        if item.payee.startswith('Transfer'):
+            continue
 
         if item.outflow > threshold:
             large_outflows.append(item)
-        elif item.inflow > threshold:
-            large_inflows.append(item)
+        elif item.inflow > 0:
+            inflows.append(item)
 
     with open(os.path.abspath(os.path.expanduser(output_file)), 'w') as handle:
         handle.write('\n'.join(output_lines))
@@ -60,25 +69,32 @@ def main(csv_file, output_file, from_date, to_date, threshold):
     if large_outflows:
         print 'Large Purchases:'
         for item in large_outflows:
-            print '  %04d  %s' % (item.line_number, item.line)
+            print '{item.date} : {item.account} : {item.payee} : -${item.outflow} : {item.memo}'.format(
+                item=item
+            )
     else:
         print 'No Large Purchases'
 
-    if large_inflows:
+    if inflows:
         print
-        print 'Large Sales:'
-        for item in large_inflows:
-            print '  %04d  %s' % (item.line_number, item.line)
+        print 'Deposits:'
+        for item in inflows:
+            print '{item.date} : {item.account} : {item.payee} : +${item.inflow} : {item.memo}'.format(
+                item=item
+            )
     else:
-        print 'No Large Sales'
+        print 'No Deposits'
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description=__doc__)
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
     parser.add_argument('csv_file')
     parser.add_argument('output_file')
-    parser.add_argument('from_date', type=get_date)
-    parser.add_argument('to_date', type=get_date)
+    parser.add_argument('from_date', type=get_date, help='inclusive date with format="DD/MM/YYYY"')
+    parser.add_argument('to_date', type=get_date, help='inclusive date with format="DD/MM/YYYY"')
     parser.add_argument('--threshold', type=float, default=500.0)
     args = parser.parse_args()
     main(**args.__dict__)
